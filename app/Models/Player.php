5ec32d2b03c7984d77e3752b3f68bef44346780f
Sophie Stammers <sophie.stammers@gmail.com>
2020-12-29 20:09:22 +0000

    Model: Player model with increment method

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Player extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["name", "play_count"];
+
+    public function incrementPlayCount()
+    {   
+        $this->play_count = $this->play_count + 1;
+        return $this->play_count;   
+    }
 }
